7b2d84d9751dc55ed7a3c0615e030122e121c896
John Wood <john@johnpwood.net>
2012-04-11 15:22:03 -0500

    Use assert_raise instead of assert_raises

--- test/short_url_test.rb
+++ test/short_url_test.rb
@@ -42,7 +42,7 @@ END
   <error>The short URL domain is not available</error>
 </errors>
 END
-    assert_raises SignalApi::ApiException do
+    assert_raise SignalApi::ApiException do
       SignalApi::ShortUrl.create("http://www.google.com", "ix.ly")
     end
   end
