44f7bd6791cd5beb2e8b93cc7efcc7a0d6e4c415
John Wood <john@johnpwood.net>
2012-04-16 08:52:38 -0500

    Add method to add users to a segment

--- lib/signal_api/segment.rb
+++ lib/signal_api/segment.rb
@@ -6,6 +6,28 @@ module SignalApi
     STATIC = "SEGMENT"
   end
 
+  # Represents a user to be added to a segment
+  class SegmentUser
+    attr_reader :mobile_phone, :email_address, :user_data
+
+    # Create a new segment on the Signal platform.
+    #
+    # @param [String] identifying_attribute The mobile phone or email address of the user
+    # @param [Hash] user_data <b>Optional</b> A collection of key/value pairs to store along
+    #                         with this user's segment record for later use
+    def initialize(identifying_attribute, user_data={})
+      if Phone.valid?(identifying_attribute)
+        @mobile_phone = identifying_attribute
+      elsif EmailAddress.valid?(identifying_attribute)
+        @email_address = identifying_attribute
+      else
+        raise InvalidParameterException.new("identifying_attribute must be a valid mobile phone number or email address")
+      end
+
+      @user_data = user_data unless user_data.empty?
+    end
+  end
+
   # Create, manage, and add users to a segment.
   class Segment < SignalHttpApi
 
@@ -25,7 +47,7 @@ module SignalApi
     attr_reader :segment_type
 
 
-    def initialize(id, name, description, segment_type, account_id)
+    def initialize(id, name=nil, description=nil, segment_type=nil, account_id=nil)
       @name = name
       @description = description
       @id = id
@@ -73,6 +95,48 @@ module SignalApi
         new(data['id'], data['name'], data['description'], lookup_segment_type(data['filter_group_type_id']), data['account_id'])
       else
         handle_api_failure(response)
+      end
+    end
+
+    # Add mobile phone numbers to a segment.
+    #
+    # @param [Array<SegmentUser>] segment_users An array of SegmentUsers to add to the segment
+    # @return [Hash] A hash containing some stats regarding the operation
+    def add_users(segment_users)
+      if segment_users.blank?
+        raise InvalidParameterException.new("An array of SegmentUser objects must be provided")
+      end
+
+      builder = Builder::XmlMarkup.new
+      body = builder.users(:type => :array) do |users|
+        segment_users.each do |segment_user|
+          users.user do |user|
+            user.mobile_phone(segment_user.mobile_phone) if segment_user.mobile_phone
+            user.email(segment_user.email_address) if segment_user.email_address
+            user.user_data(segment_user.user_data) if segment_user.user_data
+          end
+        end
+      end
+
+      SignalApi.logger.info "Attempting to add users to segment #{@id}"
+      response = self.class.post("/api/filter_segments/#{@id}/update.xml",
+                                 :body => body,
+                                 :format => :xml,
+                                 :headers => self.class.common_headers)
+
+      if response.code == 200
+        data = response.parsed_response['subscription_list_segment_results']
+
+        if data['users_not_found'] && data['users_not_found']['user_not_found']
+          SignalApi.logger.warn data['users_not_found']['user_not_found'].join(", ")
+        end
+
+        { :total_users_processed => (data['total_users_processed'] || 0).to_i,
+          :total_users_added     => (data['total_users_added'] || 0).to_i,
+          :total_users_not_found => (data['total_users_not_found'] || 0).to_i,
+          :total_duplicate_users => (data['total_duplicate_users'] || 0).to_i }
+      else
+        self.class.handle_api_failure(response)
       end
     end
 
--- test/segment_test.rb
+++ test/segment_test.rb
@@ -13,6 +13,27 @@ class SegmentTest < Test::Unit::TestCase
   end
 
   #
+  # SegmentUser
+  #
+  should "raise an error if trying to create a SegmentUser with an identifying attribute that is not a mobile phone number or email address" do
+    assert_raise SignalApi::InvalidParameterException do
+      SignalApi::SegmentUser.new("foo")
+    end
+  end
+
+  should "be able to create a SegmentUser with a mobile phone and user data" do
+    segment_user = SignalApi::SegmentUser.new("3125551212", :foo_1 => 'bar_1', :foo_2 => 'bar_2')
+    assert_equal "3125551212", segment_user.mobile_phone
+    assert_equal Hash[:foo_1 => 'bar_1', :foo_2 => 'bar_2'], segment_user.user_data
+  end
+
+  should "be able to create a SegmentUser with an email address and user data" do
+    segment_user = SignalApi::SegmentUser.new("john@test.com", :foo_1 => 'bar_1', :foo_2 => 'bar_2')
+    assert_equal "john@test.com", segment_user.email_address
+    assert_equal Hash[:foo_1 => 'bar_1', :foo_2 => 'bar_2'], segment_user.user_data
+  end
+
+  #
   # create
   #
   should "raise an error if any of the required parameters are missing" do
@@ -56,6 +77,67 @@ END
 END
     assert_raise SignalApi::ApiException do
       SignalApi::Segment.create("Segment 1", "Test segment 1", SignalApi::SegmentType::STATIC)
+    end
+  end
+
+  #
+  # add_users
+  #
+  should "raise an error if no segment users were passed in" do
+    assert_raise SignalApi::InvalidParameterException do
+      segment = SignalApi::Segment.new(1)
+      segment.add_users(nil)
+    end
+  end
+
+  should "be able to add a collection of segment users to a static segment" do
+    FakeWeb.register_uri(:post, SignalApi::SignalHttpApi::BASE_URI + '/api/filter_segments/1/update.xml', :content_type => 'application/xml', :status => ['200', 'Success'], :body => <<-END)
+<subscription_list_segment_results>
+  <users_not_found>
+    <user_not_found>user with email john@test.com not found</user_not_found>
+    <user_not_found>user with email bill@test.com not found</user_not_found>
+  </users_not_found>
+  <total_users_processed>4</total_users_processed>
+  <total_users_added>2</total_users_added>
+  <total_users_not_found>2</total_users_not_found>
+  <total_duplicate_users>0</total_duplicate_users>
+</subscription_list_segment_results>
+END
+
+    segment_users = [
+      SignalApi::SegmentUser.new("3125551212", :foo_1 => 'bar 1', :foo_2 => 'bar 2'),
+      SignalApi::SegmentUser.new("john@test.com", :foo_3 => 'bar 3', :foo_4 => 'bar 4'),
+      SignalApi::SegmentUser.new("3125551213"),
+      SignalApi::SegmentUser.new("bill@test.com")
+    ]
+
+    segment = SignalApi::Segment.new(1)
+    results = segment.add_users(segment_users)
+    assert_equal 2, results[:total_users_added]
+    assert_equal 2, results[:total_users_not_found]
+    assert_equal 0, results[:total_duplicate_users]
+    assert_equal 4, results[:total_users_processed]
+  end
+
+  should "raise an exception if unable to add users to the segment" do
+    FakeWeb.register_uri(:post, SignalApi::SignalHttpApi::BASE_URI + '/api/filter_segments/1/update.xml', :content_type => 'application/xml', :status => ['422', 'Success'], :body => <<-END)
+<?xml version="1.0" encoding="UTF-8"?>
+<error>
+  <request>http://textme.dev/api/filter_segments/63/update.xml</request>
+  <message>trying access invalid filter group</message>
+</error>
+END
+
+    segment_users = [
+      SignalApi::SegmentUser.new("3125551212", :foo_1 => 'bar 1', :foo_2 => 'bar 2'),
+      SignalApi::SegmentUser.new("john@test.com", :foo_3 => 'bar 3', :foo_4 => 'bar 4'),
+      SignalApi::SegmentUser.new("3125551213"),
+      SignalApi::SegmentUser.new("bill@test.com")
+    ]
+
+    segment = SignalApi::Segment.new(1)
+    assert_raise SignalApi::ApiException do
+      segment.add_users(segment_users)
     end
   end
 
